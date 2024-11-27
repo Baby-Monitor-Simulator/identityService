64abd4e66779190325526ef666ae012832a1a3fc
ZakariaICT <99720725+ZakariaICT@users.noreply.github.com>
2024-11-27 10:53:34 +0100

    loginrequest changed username to email

--- src/main/java/com/babymonitor/identity/models/LoginRequest.java
+++ src/main/java/com/babymonitor/identity/models/LoginRequest.java
@@ -4,8 +4,8 @@ public class LoginRequest {
     private String email;
     private String password;
 
-    public LoginRequest(String username, String password) {
-        this.username = username;
+    public LoginRequest(String email, String password) {
+        this.email = email;
         this.password = password;
     }
 
